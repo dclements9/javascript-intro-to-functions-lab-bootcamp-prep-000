fb4b4031b8a723e460868ff584de0ac29a5a05cd
dclements <dclements@protonmail.com>
2018-10-06 23:57:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,23 +6,15 @@ function whisper(string){
   return string.toLowerCase()
 }
 
-// Remove unnecessary if statements
-
 function logShout(string){
-  var stringUppercase = string
-  if (stringUppercase.toUpperCase() === stringUppercase){
-    console.log(stringUppercase)
-  }else{
-  stringUppercase = stringUppercase.toUpperCase()
-  console.log(stringUppercase)
-  }
+
+  console.log(string.toUpperCase())
+
 }
 
 function logWhisper(string){
     var stringLowercase = string
-  if (stringLowercase.toLowerCase() === stringLowercase){
-    console.log(stringLowercase)
-  }else{
+    
   stringLowercase = stringLowercase.toLowerCase()
   console.log(stringLowercase)
   }
